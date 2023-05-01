c3dbaf49d3fd84e725ca09db77dcaf937cdd64b3
Prathamesh Tamhane <prathameshdt@gmail.com>
2023-05-01 00:49:49 -0400

    Minor changes to CV, talks page

--- talkmap.py
+++ talkmap.py
@@ -13,17 +13,15 @@
 
 import glob
 import getorg
-from geopy import Nominatim
+from geopy.geocoders import Nominatim
 
 g = glob.glob("*.md")
 
-
-geocoder = Nominatim()
+geolocator = Nominatim(user_agent="mapForPersonalWebsite")
 location_dict = {}
 location = ""
 permalink = ""
 title = ""
-
 
 for file in g:
     with open(file, 'r') as f:
@@ -33,15 +31,11 @@ for file in g:
             lines_trim = lines[loc_start:]
             loc_end = lines_trim.find('"')
             location = lines_trim[:loc_end]
-                            
-           
-        location_dict[location] = geocoder.geocode(location)
-        print(location, "\n", location_dict[location])
 
+        location_dict[location] = geolocator.geocode(location)
+        print(location, "\n", location_dict[location])
 
 m = getorg.orgmap.create_map_obj()
 getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
-
-
 
 
